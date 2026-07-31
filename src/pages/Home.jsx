a2519bc5d29de781a64c5ba3ec05e91a7b1eda8a
James Clarke <james.clarke@amulethotkey.com>
2026-07-31 21:59:22 +0100

    settled on a much more simplistic design for the homepage - next I will look into nicer fonts, styling and reactive components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,63 +6,52 @@ export default function Home() {
         <div id="home">
 
                 <div id="intro">
-
-                    <img id="hero" src="hero-img.jpeg" />
-
-                    <h1 id="title">James Clarke</h1>
-
-                    <p id="intro-text">
-                        My name is James, I'm a student at the University Exeter; an engineer at Amulet 
-                        Hotkey; and an athlete at EUMHC.
-                    </p>
-
+                    <div id="intro-text">
+                        <h1>Hi!, I'm James Clarke</h1>
+                        <p>
+                            Software Engineer, Student and Athlete.
+                        </p>
+                    </div>
+                    <img id="intro-img" src="favicon.png" />
                 </div>
+                <div id="rows">
+                    <div id="row1">
+                        <p className="block">
+                            I am student at the University of Exeter studying BSc Computer Science with
+                            Industrial Experience.
 
-                <div id="texts">
+                            I am a student software engineer at Amulet Hotkey helping to develop embedded 
+                            systems and automated tooling.
+                        </p>
+                        <p className="block">
+                            Average grade pp 64% having completed key modules: Machine Learning, C Family, 
+                            Object Oriented Programming and Web Development.
 
-
-                    <p className="block">
-                        I am student at the University of Exeter studying BSc Computer Science with
-                        Industrial Experience.
-
-                        I am a student software engineer at Amulet Hotkey helping to develop embedded 
-                        systems and automated tooling.
-                    </p>
-
-                    <p className="block">
-                        Average grade pp 64% having completed key modules: Machine Learning, C Family, 
-                        Object Oriented Programming and Web Development.
-
-                        I am an active member of both Exeter CS Society and Exeter Cybersecurity Society,
-                        having attended Cyberus summer school as a representative for the University.
-                    </p>
-
-                    <p className="block">
-                        I have a strong portfolio of personal projects and hackathon entries; built from 
-                        a passion for creating amongst a number of dsciplines including AI, cybersecurity,
-                        embedded systems and blockchain technologies.
-                    </p>
-
-
-
-                    <p className="block">
-                        Over the last 2 years I have played for EUMHC, having been lucky enough to win a BUCS
-                        championship with the second team and (sort of) win promotion to the national prem 
-                        with the first team.
-                    </p>
-
-                    <p className="block">
-                        Hockey has been one of the most formative experiences of my life, and much of my 
-                        professional mentality has been derived from lessons first taught on the pitch.
-                    </p>
-
-                    <p className="block">
-                        I will soon be pursuing a project relating my work with embedded systems and 
-                        ML algorithms to my experience with the role of data in hockey
-                    </p>
-
+                            I am an active member of both Exeter CS Society and Exeter Cybersecurity Society,
+                            having attended Cyberus summer school as a representative for the University.
+                        </p>
+                        <p className="block">
+                            I have a strong portfolio of personal projects and hackathon entries; built from 
+                            a passion for creating amongst a number of dsciplines including AI, cybersecurity,
+                            embedded systems and blockchain technologies.
+                        </p>
+                    </div>
+                    <div id="row2">
+                        <p className="block">
+                            Over the last 2 years I have played for EUMHC, having been lucky enough to win a BUCS
+                            championship with the second team and (sort of) win promotion to the national prem 
+                            with the first team.
+                        </p>
+                        <p className="block">
+                            Hockey has been one of the most formative experiences of my life, and much of my 
+                            professional mentality has been derived from lessons first taught on the pitch.
+                        </p>
+                        <p className="block">
+                            I will soon be pursuing a project relating my work with embedded systems and 
+                            ML algorithms to my experience with the role of data in hockey
+                        </p>
+                    </div>
                 </div>
-
         </div>
 )
 }
