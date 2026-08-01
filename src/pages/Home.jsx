ed464f477f8b58aaecae5ba96e44a4983392d22e
James Clarke <james.clarke@amulethotkey.com>
2026-08-01 14:07:42 +0100

    bunch of stuff, home page slight rework, 2 more projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ export default function Home() {
 
     return (
         <div id="home">
-
                 <div id="intro">
                     <div id="intro-text">
                         <h1>Hi!, I'm James Clarke</h1>
@@ -18,16 +17,10 @@ export default function Home() {
                     Studying Bsc Computer Science with a year in industry at the University of Exeter, 
                     Where I am an active member in the Computer Science Society and Cybersecurity Society,
                     having represented Exeter at international events.
-                </p>
-                <p className="text">
                     Working as a Student Software Engineer at Amulet Hotkey as my year in industry, 
                     where I work hands on with embedded systems and automated business processes.
-                </p>
-                <p className="text">
                     Playing hockey at the EUMHC for both the men's first and second team over
                     the last two years, winning BUCS gold and promotion to the top flight of english hockey.
-                </p>
-                <p className="text">
                     Here you can find out what I'm working on now, view my professional experience and look at 
                     my projects and past releases. I also keep a gallery here of some of my favourite on pitch 
                     moments, enjoy!
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,9 +2,15 @@ import '../styles/projects.css'
 
 export default function() {
     return(
-        <div id="now">
+        <div id="projects">
+            <div id="intro-text">
+                <h1>My Projects!</h1>
+                <p>
+                    A collection of projects I am or have worked on
+                </p>
+            </div>
             <div className="container">
-                <div id="description">
+                <div className="description">
                     <div className="heading">
                         <h2>Fixr-Bot</h2>
                         <a href="https://github.com/JamesAlexanderC/Fixr-bot" className="button">GitHub<img src="github-dark.svg" id="github-svg" className="svg" /></a>
@@ -34,10 +40,55 @@ export default function() {
                         dedicated <a href="https://fastapi.tiangolo.com/"> FastAPI </a> API served on 
                         <a href="https://gunicorn.org/"> Gunicorn </a>, requests can be made to the API to add messages 
                         to the queue or to edit configuration details. A very simple graphical interface is served.
-                        
                     </p>
                 </div>
-                <img id="fixr-img" src="fixr-bot.png"></img>
+                <img className="project-img" id="fixr-img" src="fixr-bot.png"></img>
+            </div>
+            <div className="container">
+                <div className="description">
+                    <div className="heading">
+                        <h2>My Personal Website</h2>
+                        <a href="https://github.com/JamesAlexanderC/me" className="button">GitHub<img src="github-dark.svg" id="github-svg" className="svg" /></a>
+                        <div className="tools">
+                            <img src="react.svg" className="svg" />
+                            <img src="html5.svg" className="svg" />
+                            <img src="css.svg" className="svg" />
+                        </div>
+                    </div>
+                    <p className="text">
+                        This is my Personal Website! A simple static react app where I keep public information,
+                        my experience, projects updates and more. I've built it for fun and it is still in development.
+                    </p>
+                    <p className="text">
+                        This website was self designed and implemented in react, once finished I plan to begin self
+                        hosting and integrate interfaces for future projects and other services such as a public docker
+                        registry and git server, with custom interfaces.
+                    </p>
+                </div>
+                <img className="project-img" id="website-img" src="personal-website.png"></img>
+            </div>
+            <div className="container">
+                <div className="description">
+                    <div className="heading">
+                        <h2>Project Ate</h2>
+                        <a href="https://github.com/JamesAlexanderC/me" className="button">GitHub<img src="github-dark.svg" id="github-svg" className="svg" /></a>
+                        <div className="tools">
+                            <img src="python.svg" className="svg" />
+                            <img src="django.svg" className="svg" />
+                            <img src="react.svg" className="svg" />
+                        </div>
+                    </div>
+                    <p className="text">
+                        project Ate was a university group project I lead during second year, as a team of 7 we developed a full stack
+                        web app market place, capable of supporting multiple sellers, consumers, staff members, an analytics engine and
+                        a gamification layer. This app is not currently being developed, and is not intended for contribution, however I 
+                        have plans to build a personal fork.
+                    </p>
+                    <p className="text">
+                        We used a simple Django/React stack, and JWT auth tokens to ensure security. 
+                    </p>
+                </div>
+                <img className="project-img" id="website-img" src="personal-website.png"></img>
             </div>
         </div>
     )
